58e333ff0686f3e64944f6f2494f80c2fc00fc3a
ML2019-xiaoma <57028256+ML2019-xiaoma@users.noreply.github.com>
2020-07-16 22:51:06 +0800

    v1.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Login from '@/views/Login.vue'
+
+Vue.use(ElementUI)
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
